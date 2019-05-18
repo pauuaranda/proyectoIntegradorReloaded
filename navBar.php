2c86d1a3abbef9c89e2da6f2befe078c1da12ca2
pauuaranda <pauuaranda97@gmail.com>
2019-05-18 19:02:31 -0300

    nav(?

---
+++ navBar.php
@@ -0,0 +1,11 @@
+ 
+<!-- /*----------------------------Mobile First NavBar-------------------------------*/  -->
+<div class="container_navBar">
+<div Class ="botonLineas">
+<div class ="lineIcon"></div>
+<div class ="lineIcon"></div>
+<div class ="lineIcon"></div>
+</div>
+<div class="logo mx-auto"><a href="index.php"><img src="img/logo.svg" alt=""></a></div>
+
+</div>
