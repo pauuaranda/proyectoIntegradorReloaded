51e2a1645e55223225bb16d617cd8519398c9a81
Enzocons <enzoconstantopoulos@gmail.com>
2019-05-21 14:36:11 -0300

    slide con js funcionando

---
+++ abrir.js
@@ -0,0 +1,3 @@
+$('.menu-bar').on('click', function(){
+    $('.container').toggleClass('abrir');
+});
--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>:: Black Eye Gaming ::</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="index.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <title>:: Black Eye Gaming ::</title>
 </head>
 <body>
-    <h2>GOGOGOOGOGO</h2>
-    <p>asdfgh</p>
+    <div class="sidebar">
+        <h2>Slide de prueba</h2>
+        <ul>
+            <li><a>Componentes</a></li>
+            <li><a>Accesorios</a></li>
+        </ul>
+    </div>
+    <div class="container">
+        <img src="img/Hamburger_icon.svg.png" alt="" class="menu-bar">
+    </div>
+    <script src="abrir.js"></script>
 </body>
 </html>
