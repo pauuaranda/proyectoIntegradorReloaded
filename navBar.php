2e0db47835338f37349fef63afa8e2cf15bddbf8
pauuaranda <pauuaranda97@gmail.com>
2019-05-24 18:52:14 -0300

    mini canges nav

--- navBar.php
+++ navBar.php
@@ -1,11 +1,22 @@
  
 <!-- /*----------------------------Mobile First NavBar-------------------------------*/  -->
+<nav>
 <div class="container_navBar">
-<div Class ="botonLineas">
-<div class ="lineIcon"></div>
-<div class ="lineIcon"></div>
-<div class ="lineIcon"></div>
-</div>
-<div class="logo mx-auto"><a href="index.php"><img src="img/logo.svg" alt=""></a></div>
-<p>lalala</p>
-</div>
+        <div Class ="botonLineas">
+        <div class ="lineIcon"></div>
+        <div class ="lineIcon"></div>
+        <div class ="lineIcon"></div>
+        </div>
+        <a class="botonNavBar" href="search.php" ><img class="botonNavBar"src="img/search.svg" alt="Search"></a>
+        <div class="logo"><a href="index.php"><img src="img/logo.svg" alt=""></a></div>
+        <a class="botonNavBar" href="carrito.php" ><img class="botonNavBar"src="img/carrito.svg" alt="Shopping_Cart"></a>
+        <a class="botonNavBar" href="login.php" ><img class="botonNavBar"src="img/profile.png" alt="Profile_icon"></a>
+        </div>
+<div class="container_NavBarVertical"></div>
+        <div class="logo_Vertical"><a href="index.php"><img src="img/logo.svg" alt=""></a></div>
+        <div class="container_verticalIcons row">
+            <div class="shop_Icon">
+            <a  class="nav_Icons" href="shop.php"><img src="img/shop_icon.svg" alt="shop_icon"> </a>
+            </div> 
+        </div>
+</nav>
