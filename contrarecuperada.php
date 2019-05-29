b3e5057f9fffc114b6c2b0b173c7c54341d3954a
pauuaranda <pauuaranda97@gmail.com>
2019-05-28 23:45:53 -0300

    rec

---
+++ contrarecuperada.php
@@ -0,0 +1,27 @@
+a<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/contraRecuperada.css">
+    <title>Mission Complete</title>
+    <link rel="shortcut icon" href="img/favicon.ico">
+</head>
+<body>
+    <div>
+        <div class="logo1">
+            <img src="img/logo.svg" alt="">
+        </div>
+        <section>
+            <article class="articleR">
+                <h2>Contraseña Restablecida</h2>
+                <hr>
+                <p>Su contraseña se ha restablecido correctamente</p> <br>
+                <a type="button" class="botonenviar" href="login.php">Aceptar</a>
+            </article>
+        </section>
+    </div>
+    
+</body>
+</html>
